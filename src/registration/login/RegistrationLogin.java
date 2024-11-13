75163482baf4892641c44391203e6bdfab8a4d4f
r0fing <xuankien0712@gmail.com>
2024-11-14 01:21:59 +0700

    November 14th, 2024

---
+++ src/registration/login/RegistrationLogin.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package registration.login;
+
+/**
+ *
+ * @author HLC_2021
+ */
+public class RegistrationLogin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Register rf = new Register();
+        rf.setVisible(true);
+    }
+    
+}
